6960b9937e68669c22a7fe99145f7d26ced85645
changlintsai <42146530+changlintsai@users.noreply.github.com>
2018-08-10 22:19:26 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(name='gym_maze',
       install_requires=['gym']  
      )
 
-setup(name='gym_maze_2',
+setup(name='gym_maze_1',
       version='0.0.1',
       install_requires=['gym']
      )
 
-setup(name='gym_maze_3',
+setup(name='gym_maze_2',
       version='0.0.1',
       install_requires=['gym']
      )
