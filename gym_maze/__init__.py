5fdfe360b327e33bc8d0ffb76e15952f539f018e
changlintsai <42146530+changlintsai@users.noreply.github.com>
2018-08-10 22:22:15 +0800

    Update __init__.py

--- gym_maze/__init__.py
+++ gym_maze/__init__.py
@@ -4,13 +4,3 @@ register(
     id='maze-v0',
     entry_point='gym_maze.envs:MazeEnv',
 )
-
-register(
-    id='maze-v1',
-    entry_point='gym_maze_1.envs:MazeEnv',
-)
-
-register(
-    id='maze-v2',
-    entry_point='gym_maze_2.envs:MazeEnv',
-)
